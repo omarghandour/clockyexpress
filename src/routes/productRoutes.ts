0b3b785445af677afa3b93772eb4707ad61e9f9a
Omar <omarkhaled9913@gmail.com>
2024-12-26 18:23:45 +0200

    add auth middleware to cart product route for enhanced security

--- src/routes/productRoutes.ts
+++ src/routes/productRoutes.ts
@@ -68,7 +68,11 @@ router
   .delete(authMiddleware, removeProduct)
   .put(authMiddleware, updateProduct);
 // cart
-router.route("/cart/:id").get(cartProduct).post(addToCart).put(productQuantity);
+router
+  .route("/cart/:id")
+  .get(authMiddleware, cartProduct)
+  .post(addToCart)
+  .put(productQuantity);
 router.route("/cart/add/one").post(addProductToCart);
 router.route("/cart/all/:id").post(addToCartAll);
 // checkouts
