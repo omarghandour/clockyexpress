14a32325b54a036699d01f05e6d2ee5c9dd41b65
Omar <omarkhaled9913@gmail.com>
2025-02-12 03:11:42 +0200

    test

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -66,6 +66,7 @@ const getProducts = async (req: Request, res: Response) => {
       caseColor,
       dialColor,
     } = req.query as GetProductParams;
+    console.log(minPrice, maxPrice);
 
     const sortOrder = order === "desc" ? -1 : 1;
     const allowedSortFields = ["name", "price", "createdAt", "brand"];
@@ -169,7 +170,7 @@ const getNewArrivals = async (req: Request, res: Response) => {
   try {
     const newArrivals = await Product.find()
       .sort({ createdAt: -1 }) // Sort by createdAt in descending order
-      .limit(5); // Limit the result to 5 items
+      .limit(10); // Limit the result to 5 items
     res.status(200).json(newArrivals);
   } catch (error) {
     res.status(500).json({ message: "Failed to retrieve new arrivals" });
