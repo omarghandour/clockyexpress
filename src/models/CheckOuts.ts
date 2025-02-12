4ed213db8aa93b7d137664b26f32a51099e29f15
Omar <omarkhaled9913@gmail.com>
2025-02-12 04:56:05 +0200

    add lastName to t he checkout

--- src/models/CheckOuts.ts
+++ src/models/CheckOuts.ts
@@ -67,6 +67,7 @@ const CheckoutSchema: Schema = new Schema(
     },
     shippingAddress: {
       fullName: { type: String, required: true },
+      lastName: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
       governorate: { type: String, required: true },
